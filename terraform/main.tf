9faa41f449fce639ed787763a4f1defa69b65a2e
AJ Barea <ajbareaa@gmail.com>
2025-07-10 15:14:41 -0400

    chore: update AWS provider version to ~> 6.2.0 in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.70"
+      version = "~> 6.2.0"
     }
     local = {
       source  = "hashicorp/local"
